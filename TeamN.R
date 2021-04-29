b86e1dbdc8093d1d086f247af9cad774413ead0c
Anisha Gademsky <ang324@lehigh.edu>
2021-04-28 21:09:15 -0400

    Changes

--- TeamN.R
+++ TeamN.R
@@ -6,11 +6,10 @@ my_data1 <- my_data[which(my_data$`Academic Year`>"71-72"),]
 my_data1 <- my_data[which(my_data$`Academic Year`<"10-11"),]
 
 my_data1 <- my_data
-ggplot(data = my_data1)+
+plot <- ggplot(data = my_data1)+
   geom_point(aes(x = `Academic Year`, y= `One-Year % Change`))
  
-
-
+plot + theme (text=element_text(size = 7))
 
 
 
