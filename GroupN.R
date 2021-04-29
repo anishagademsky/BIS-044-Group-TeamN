a3b3d94736dd9e3dc592a9bcc5b360c7d8a7f272
Anisha Gademsky <ang324@lehigh.edu>
2021-04-29 17:25:51 -0400

    Cleaned data and graph

--- GroupN.R
+++ GroupN.R
@@ -5,10 +5,9 @@ library(ggplot2)
 my_data1 <- my_data[which(my_data$`Academic Year`>"71-72"),]
 my_data1 <- my_data[which(my_data$`Academic Year`<"10-11"),]
 
-my_data1 <- my_data
 plot <- ggplot(data = my_data1)+
   geom_point(aes(x = `Academic Year`, y= `One-Year % Change`))
- 
+plot
 plot + theme (text=element_text(size = 7))
 
 
