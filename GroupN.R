cc121557596d64116c93473974cffcfd52648d84
Anisha Gademsky <ang324@lehigh.edu>
2021-04-29 18:13:05 -0400

    Updated R Script

--- GroupN.R
+++ GroupN.R
@@ -6,9 +6,12 @@ my_data1 <- my_data[which(my_data$`Academic Year`>"71-72"),]
 my_data1 <- my_data[which(my_data$`Academic Year`<"10-11"),]
 
 plot <- ggplot(data = my_data1)+
-  geom_point(aes(x = `Academic Year`, y= `One-Year % Change`))
+  geom_bar(aes(x = `Academic Year`, y= `One-Year % Change`),stat="identity",position = "dodge")
 plot
-plot + theme (text=element_text(size = 7))
+plot + theme (text=element_text(size = 7)) + 
+xlab("Academic Years from 2000 to 2010") + ylab("One Year % Change in Tuition")+
+ggtitle("Change in Tuition vs Academic Year")
+
 
 
 
